426b636403b7596daae6e7684b314d5608a216e3
taejun <joon5006@naver.com>
2021-01-25 21:41:28 +0900

    feat

--- config/settings.py
+++ config/settings.py
@@ -100,14 +100,20 @@ if DEBUG:
 else:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "HOST": os.environ.get("RDS_HOST"),
-            "NAME": os.environ.get("RDS_NAME"),
-            "USER": os.environ.get("RDS_USER"),
-            "PASSWORD": os.environ.get("RDS_PASSWORD"),
-            "PORT": "5432",
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "HOST": os.environ.get("RDS_HOST"),
+#         "NAME": os.environ.get("RDS_NAME"),
+#         "USER": os.environ.get("RDS_USER"),
+#         "PASSWORD": os.environ.get("RDS_PASSWORD"),
+#         "PORT": "5432",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
