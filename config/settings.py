943a9ddeb6ab023652d3a8dafdfee703093b6881
taejun <joon5006@naver.com>
2021-01-26 22:59:05 +0900

    feat: postgresql 설정

--- config/settings.py
+++ config/settings.py
@@ -101,20 +101,14 @@ if DEBUG:
 else:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+            "ENGINE": "django.db.backends.postgresql",
+            "HOST": os.environ.get("RDS_HOST"),
+            "NAME": os.environ.get("RDS_NAME"),
+            "USER": os.environ.get("RDS_USER"),
+            -"PASSWORD": os.environ.get("RDS_PASSWORD"),
+            "PORT": "5432",
         }
     }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "HOST": os.environ.get("RDS_HOST"),
-#         "NAME": os.environ.get("RDS_NAME"),
-#         "USER": os.environ.get("RDS_USER"),-
-#         "PASSWORD": os.environ.get("RDS_PASSWORD"),
-#         "PORT": "5432",
-#     }
-# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
