dfbf0888d87a7917eb03de4fb8cbce1fe5b01c99
taejun <joon5006@naver.com>
2021-01-26 23:45:44 +0900

    feat

--- config/settings.py
+++ config/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "tmeb1zm+7a0p)5l2n-h4b5%732^va=1*!l9rfi*na#e#n&7h&8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
-
+DEBUG = True
+# bool(os.environ.get("DEBUG"))
 
 ALLOWED_HOSTS = ["*"]
 APPEND_SLASH = False
@@ -90,7 +90,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG:
+if DEBUG is False:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
